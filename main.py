06cd99b94bd1dfbaad8548687833f867b88344d1
Bonga Ishigaki Maseko <102910937+Ishigxki@users.noreply.github.com>
2026-03-11 14:18:22 +0200

    Added endpoint (GRUD 60%)

--- main.py
+++ main.py
@@ -1,28 +1,53 @@
 ## main.py
 from fastapi import FastAPI
+from pydantic import BaseModel
 from database import connection,cursor
 
-# Create a FastAPI app instance
+
 app = FastAPI()
+# Create a FastAPI app instance
+class Task(BaseModel):
+    title: str
 
 @app.get("/")
 def home():
-    return {"message":"Gear 5 Api running"}
+    return {"message": "Gear 5 API running"}
 
 # Define a GET endpoint at the root URL "/"
 @app.get("/tasks")
 def get_tasks():
     cursor.execute("SELECT * FROM tasks;")
     rows = cursor.fetchall()
+    tasks = []
+    for row in rows:
+        tasks.append({"id": row[0],"title":row[1]})
+
+    return {"tasks": tasks}
+@app.get("/tasks/{id}")
+def get_tasks_by_id(id:int):
+    cursor.execute("SELECT * FROM tasks WHERE id=%s;",(id,))
+    rows= cursor.fetchone()
     return {"tasks": rows}
+
+@app.put("/tasks/{id}")
+def update_task(id:int):
+    cursor.execute("UPDATE tasks SET * WHERE id=%s;",(id,))
+    
+
 @app.post("/tasks")
-def create_task(task:str):
-    cursor.execute("INSERT INTO tasks (title) VALUES (%s) RETURNING id;",(task,))
+def create_task(task: Task):
+    cursor.execute(
+        "INSERT INTO tasks (title) VALUES (%s);",
+        (task.title,)
+    )
     connection.commit()
     return {"message": "Task created"}
 
-@app.delete("/tasks")
-def remove_task(task:str):
-    cursor.execute("DELETE FROM tasks WHERE title = %s;", (task,))
+@app.delete("/tasks/{id}")
+def delete_task(id: int):
+    cursor.execute(
+        "DELETE FROM tasks WHERE id = %s;",
+        (id,)
+    )
     connection.commit()
     return {"message": "Task deleted"}
